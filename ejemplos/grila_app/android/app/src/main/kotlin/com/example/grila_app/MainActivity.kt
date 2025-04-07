616bd8fcb84b9d23c6e035599da513517c411c73
HuascarFedor <huascar.fedor@gmail.com>
2025-04-07 15:13:08 -0400

    added grilla ejemplos

---
+++ ejemplos/grila_app/android/app/src/main/kotlin/com/example/grila_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.grila_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ejemplos/grila_app/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Ejemplo de GridView')),
+        body: const Ejemplo1(),
+      ),
+    );
+  }
+}
+
+class Ejemplo1 extends StatelessWidget {
+  const Ejemplo1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      // Numero de columnas en la cuadricula
+      crossAxisCount: 3,
+      // Espacio entre los elementos verticalmenete
+      crossAxisSpacing: 10,
+      // Espacio entre los elementos horizontalmente
+      mainAxisSpacing: 10,
+      // Espacio entre los bordes de la pantalla y el grid
+      padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+      // Tamaño de los elementos
+      childAspectRatio: 1.0,
+      // Lista de widgets que se mostraran en el grid
+      children: List.generate(10, (index) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.blue[100 * (index % 9 + 1)],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Text(
+              'Item $index',
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
