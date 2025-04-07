3cb35f6b76fd302bdcff03a85de42f97bb4783b4
HuascarFedor <huascar.fedor@gmail.com>
2025-04-07 15:37:43 -0400

    added grilla ejemplos 1,2,3 y 4

--- ejemplos/grila_app/lib/main.dart
+++ ejemplos/grila_app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text('Ejemplo de GridView')),
-        body: const Ejemplo2(),
+        body: const Ejemplo4(),
       ),
     );
   }
@@ -78,6 +78,66 @@ class Ejemplo2 extends StatelessWidget {
         Image.network('https://picsum.photos/200/200?random=4'),
         Image.network('https://picsum.photos/200/200?random=5'),
         Image.network('https://picsum.photos/200/200?random=6'),
+      ],
+    );
+  }
+}
+
+class Ejemplo3 extends StatelessWidget {
+  const Ejemplo3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 1.0,
+      ),
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Container(
+          color: Colors.green[100 * (index % 9 + 1)],
+          child: Center(
+            child: Text(
+              'Item $index',
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Ejemplo4 extends StatelessWidget {
+  const Ejemplo4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          leading: Icon(Icons.map),
+          title: Text('Mapa'),
+          subtitle: Text('Abre el mapa'),
+        ),
+        ListTile(
+          leading: Icon(Icons.photo),
+          title: Text('Albun'),
+          subtitle: Text('Tus fotos'),
+        ),
+        ListTile(
+          leading: Icon(Icons.phone),
+          title: Text('Telefono'),
+          subtitle: Text('Llamadas recientes'),
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('Configuracion'),
+          subtitle: Text('Ajustes de la aplicacion'),
+        ),
       ],
     );
   }
