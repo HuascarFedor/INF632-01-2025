36ea30565f0caada8307df863149ba3786c751b2
HuascarFedor <huascar.fedor@gmail.com>
2025-05-16 15:21:44 -0400

    cacho app 3

--- ejemplos/cacho_app/lib/screens/game_screen.dart
+++ ejemplos/cacho_app/lib/screens/game_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:math';
 
-import 'package:cacho_app/widgets/dice_widget.dart';
-
-import '../models/game_model.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/category_selector.dart';
+import '../widgets/dice_widget.dart';
+import '../models/game_model.dart';
+import '../utils/score_calculator.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
@@ -57,6 +59,37 @@ class _GameScreenState extends State<GameScreen> {
     });
   }
 
+  void selectCategory(String category) {
+    int score = calculateScore(category, dice);
+    setState(() {
+      // Asignar el puntaje a la hoja del jugador actual
+      players[currentPlayer].scoreSheet[category] = score;
+
+      //Actualizar el puntaje total
+      players[currentPlayer].totalScore;
+
+      //Reiniciar el estado del turno
+      awaitingCategorySelection = false;
+      rollsLeft = 3;
+      isLocked = [false, false, false, false, false];
+      dice = [1, 2, 3, 4, 5];
+
+      //Verificar si todos los jugadores completaron sus categorias
+      bool gameOver = players.every(
+        (player) =>
+            player.scoreSheet.values.where((v) => v != null).length ==
+            player.scoreSheet.length,
+      );
+
+      if (gameOver) {
+        gameEnded = true;
+      } else {
+        // Cambiar al siguiente jugador solo si el juego no terminó
+        currentPlayer = (currentPlayer + 1) % players.length;
+      }
+    });
+  }
+
   void toggleLock(index) {
     setState(() {
       if (rollsLeft == 3) {
@@ -81,6 +114,11 @@ class _GameScreenState extends State<GameScreen> {
               ElevatedButton(
                 onPressed: rollDice,
                 child: Text('Lanzar dados($rollsLeft intentos)'),
+              ),
+              CategorySelector(
+                categories: categories,
+                scoreSheet: players[currentPlayer].scoreSheet,
+                onSelectCategory: selectCategory,
               ),
             ],
           ),
