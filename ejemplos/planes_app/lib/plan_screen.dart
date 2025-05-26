94ad7fee0d4fbe2cd3d78341d67e6e55484afff8
HuascarFedor <huascar.fedor@gmail.com>
2025-05-26 14:52:18 -0400

    added planes app 2

--- ejemplos/planes_app/lib/plan_screen.dart
+++ ejemplos/planes_app/lib/plan_screen.dart
@@ -7,12 +7,44 @@ class PlanScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Manejo de Planes')),
-      // body: ,
+      body: _buildList(),
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         tooltip: 'Agregar Plan',
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView(
+      children: <Widget>[
+        for (int i = 0; i < 5; i++) ...[
+          Container(
+            margin: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(8.0),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.black26, width: 1.0),
+              ),
+            ),
+            child: ListTile(
+              title: Text(
+                'Plan ${i + 1}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18.0,
+                ),
+              ),
+              onTap: () {},
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete, color: Colors.redAccent),
+              ),
+            ),
+          ),
+        ],
+      ],
     );
   }
 }
