3c2454a73e83ca570e06473ed3cdf029e0640e10
HuascarFedor <huascar.fedor@gmail.com>
2025-06-09 15:12:49 -0400

    app planes con logger

--- ejemplos/planes_app/lib/repositories/todo_repository.dart
+++ ejemplos/planes_app/lib/repositories/todo_repository.dart
@@ -1,7 +1,27 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:logger/logger.dart';
 
 import '../models/plan.dart';
+
+class Log {
+  static final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 2, // Muestra 2 niveles de llamadas en la pila
+      errorMethodCount: 5, // Muestra 5 niveles para errores
+      lineLength: 120, // Longitud máxima de línea
+      colors: true, // Salida coloreada
+      printEmojis: true, // Emojis en mensajes
+      //printTime: true, // Imprimir timestamps
+    ),
+  );
+
+  static void debug(dynamic message) => _logger.d(message);
+  static void info(dynamic message) => _logger.i(message);
+  static void warning(dynamic message) => _logger.w(message);
+  static void error(dynamic message) => _logger.e(message);
+  //static void verbose(dynamic message) => _logger.v(message);
+}
 
 class TodoRepository {
   Database? _database;
@@ -25,6 +45,9 @@ class TodoRepository {
   }
 
   Future<List<Plan>> getAllPlans() async {
+    final dbPath = await getDatabasesPath();
+    Log.info("Archivo de base de datos: $dbPath");
+
     final db = await database;
     final List<Map<String, dynamic>> planMaps = await db.query('Plans');
     return planMaps.map((planMap) => Plan.fromMap(planMap)).toList();
